9369788fc09c5f564ad5d73206c9c3245fc6a0c2
Michael Andersen <mian@kfst.dk>
2021-07-09 13:29:07 +0200

    Refactor import order and add author doc

--- argument_permutator.py
+++ argument_permutator.py
@@ -1,14 +1,24 @@
+"""A argument permutation generator for automated benchmarking and testing.
+
+.. module:: argument_permutator.py
+    :platform: GNU/Linux
+    :synopsis: Creates a generator for generating permutation of keyword arguments based on
+               inputs, that is returned as a dictionary that can be used as arguments for
+               a function. As an example see demo.py.
+
+.. moduleauthor:: Michael Andersen <gosuckadeadcow+github@gmail.com>
+"""
 from collections.abc import Generator
-from itertools import product
 from functools import reduce
-import operator
+from itertools import product
+from operator import mul
 
 
 class ArgumentPermutator(Generator):
     def __init__(self, **kwargs):
-        self.keys = kwargs.keys()
-        self.length = reduce(operator.mul, map(len, kwargs.values()))
         self.arguments = product(*kwargs.values())
+        self.keys = kwargs.keys()
+        self.length = reduce(mul, map(len, kwargs.values()))
 
     def send(self, ignored_args=None):
         try:
