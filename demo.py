72c8317c9650c7ed2f9b43a957df86945e375e30
Michael Andersen <mian@kfst.dk>
2021-07-09 13:29:28 +0200

    Update demo.py and add len(..) example

--- demo.py
+++ demo.py
@@ -3,9 +3,11 @@ from argument_permutator import ArgumentPermutator
 ap = ArgumentPermutator(a=[1, 2], b=[True, False])
 
 
-def func(**kwargs):
+def print_args_func(**kwargs):
     print(', '.join(f'{kw}={arg}' for kw, arg in kwargs.items()))
 
 
+print(f'There is a total of {len(ap)} different argument permutations.')
+
 for argument_permutation in ap:
-    func(**argument_permutation)
+    print_args_func(**argument_permutation)
